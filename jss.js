9692ec70541cec8c22d2285c6c74352884105716
MohamedChaibi <hamma_chaibi@yahoo.fr>
2024-10-04 15:48:22 +0100

    portfolio

---
+++ jss.js
@@ -0,0 +1,22 @@
+// Initialization for ES Users
+import {
+    Dropdown,
+    initTWE,
+  } from "tw-elements";
+  
+  initTWE({ Dropdown });
+  // Initialization for ES Users
+import {
+    Collapse,
+    Dropdown,
+    initTWE,
+  } from "tw-elements";
+  
+  initTWE({ Collapse, Dropdown });
+  // Initialization for ES Users
+import {
+    Ripple,
+    initTWE,
+  } from "tw-elements";
+  
+  initTWE({ Ripple });
